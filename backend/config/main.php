822cd05b01f13ab04b33c6c2132339d88912729f
pride_zze <zze15063647762@163.com>
2018-03-29 21:57:13 +0800

    adminlye   yiidamin  路由美化

--- backend/config/main.php
+++ backend/config/main.php
@@ -11,7 +11,23 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
-    'modules' => [],
+    'modules' => [
+	"admin" => [
+        	"class" => "mdm\admin\Module",
+    	],
+    ],
+    "aliases" => [
+   	 "@mdm/admin" => "@vendor/mdmsoft/yii2-admin",
+    ],
+    'as access' => [
+    	'class' => 'mdm\admin\components\AccessControl',
+    	'allowActions' => [
+        	//这里是允许访问的action
+	        //controller/action
+    		// * 表示允许所有，后期会介绍这个
+	        '*'
+    	]
+    ],
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
@@ -37,15 +53,17 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-	/*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
 	    'rules' => [
             ],
-        ],
-	*/
-
+       ], 
+	"authManager" => [
+        "class" => 'yii\rbac\DbManager',
+        "defaultRoles" => ["guest"],
+    ],
     ],
     'params' => $params,
 ];
